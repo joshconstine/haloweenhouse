f9c01fad962904f83b398ad24f32cb9dd725fcca
joshua <joshua.constine97@gmail.com>
2023-09-23 17:54:29 -0700

    while loop is sending input

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ print(api_key)
 print("welcome to haloween house")
 import openai
 
+def send_user_input(input_text, conversation):
+    conversation.append({"role": "user", "content": input_text})
+    response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation,
+            api_key=api_key
+    )
+    return response
+
 
 # Define the conversation with system, user, and assistant messages
 conversation = [
@@ -22,4 +31,10 @@ response = openai.ChatCompletion.create(
 # Extract and print the assistant's reply
 assistant_reply = response['choices'][0]['message']['content']
 print("Assistant:", assistant_reply)
-
+# Start the while loop
+while True:
+    user_input = input("You: ")  # Gather user input
+    conversation.append({"role": "user", "content": user_input})  
+    response = send_user_input(user_input, conversation)
+    assistant_reply = response['choices'][0]['message']['content']
+    print("Assistant:", assistant_reply)
